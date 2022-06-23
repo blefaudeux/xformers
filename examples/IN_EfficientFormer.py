b04f1f8aebf904eda4e784d0a21a78a0d3ffa6a6
Benjamin Lefaudeux <benjamin.lefaudeux@pm.me>
2022-06-23 07:12:17 +0200

    handling different normalizations + a specific conv-based feedforward

--- examples/IN_EfficientFormer.py
+++ examples/IN_EfficientFormer.py
@@ -4,11 +4,11 @@
 # LICENSE file in the root directory of this source tree.
 
 
-from pathlib import Path
-
 import pytorch_lightning as pl
 import torch
-from pl_bolts.datamodules import ImagenetDataModule
+from ffcv.fields.decoders import IntDecoder, RandomResizedCropRGBImageDecoder
+from ffcv.loader import Loader, OrderOption
+from ffcv.transforms import Cutout, ToDevice, ToTensor
 from torch import nn
 from torchmetrics import Accuracy
 
@@ -31,7 +31,7 @@ class EfficientFormer(VisionTransformer):
     def __init__(
         self,
         steps,
-        learning_rate=1e-2,
+        learning_rate=5e-4,
         betas=(0.9, 0.99),
         weight_decay=0.03,
         image_size=32,
@@ -48,7 +48,6 @@ class EfficientFormer(VisionTransformer):
 
         # Generate the skeleton of our hierarchical Transformer
         # This implements a model close to the L1 suggested in "EfficientFormer" (https://arxiv.org/abs/2206.01191)
-        # but a pooling layer has been removed, due to the very small image dimensions in Cifar (32x32)
         base_hierarchical_configs = [
             BasicLayerConfig(
                 embedding=48,
@@ -57,7 +56,8 @@ class EfficientFormer(VisionTransformer):
                 stride=2,
                 padding=1,
                 seq_len=image_size * image_size // 16,
-                feedforward="Conv2DFeedforward",
+                feedforward="ConvBN",
+                normalization="skip",
             ),
             BasicLayerConfig(
                 embedding=96,
@@ -66,7 +66,8 @@ class EfficientFormer(VisionTransformer):
                 stride=2,
                 padding=1,
                 seq_len=image_size * image_size // 64,
-                feedforward="Conv2DFeedforward",
+                feedforward="ConvBN",
+                normalization="skip",
             ),
             BasicLayerConfig(
                 embedding=224,
@@ -75,7 +76,8 @@ class EfficientFormer(VisionTransformer):
                 stride=2,
                 padding=1,
                 seq_len=image_size * image_size // 256,
-                feedforward="Conv2DFeedforward",
+                feedforward="ConvBN",
+                normalization="skip",
             ),
             BasicLayerConfig(
                 embedding=448,
@@ -84,17 +86,18 @@ class EfficientFormer(VisionTransformer):
                 stride=2,
                 padding=1,
                 seq_len=image_size * image_size // 1024,
-                feedforward="Conv2DFeedforward",
+                feedforward="ConvBN",
+                normalization="skip",
             ),
         ]
 
         # Fill in the gaps in the config
         xformer_config = get_hierarchical_configuration(
             base_hierarchical_configs,
-            layernorm_style="pre",
-            use_rotary_embeddings=False,
+            layernorm_style="post",
+            use_rotary_embeddings=True,
             mlp_multiplier=4,
-            in_channels=24,  # 24 if L1, there's a stem prior to the trunk
+            in_channels=24,
         )
 
         # Now instantiate the EfficientFormer trunk
@@ -102,6 +105,7 @@ class EfficientFormer(VisionTransformer):
         config.weight_init = "moco"
 
         self.trunk = xFormer.from_config(config)
+        print(self.trunk)
 
         # This model requires a pre-stem (a conv prior to going through all the layers above)
         self.pre_stem = build_patch_embedding(
@@ -139,35 +143,38 @@ if __name__ == "__main__":
 
     # Adjust batch depending on the available memory on your machine.
     # You can also use reversible layers to save memory
-    REF_BATCH = 128
-    BATCH = 32  # lower if not enough GPU memory
+    REF_BATCH = 1024
+    BATCH = 256  # lower if not enough GPU memory
 
     MAX_EPOCHS = 50
-    NUM_WORKERS = 4
+    NUM_WORKERS = 3
     GPUS = 1
 
     torch.cuda.manual_seed_all(42)
     torch.manual_seed(42)
 
-    # We'll use a datamodule here, which already handles dataset/dataloader/sampler
-    # See https://pytorchlightning.github.io/lightning-tutorials/notebooks/lightning_examples/cifar10-baseline.html
-    # for a full tutorial
-    dm = ImagenetDataModule(
-        data_dir=Path.home()
-        / Path(
-            "Data/ImageNet/imagenet-object-localization-challenge/ILSVRC/Data/CLS-LOC"
-        ),
+    # Random resized crop
+    decoder = RandomResizedCropRGBImageDecoder((224, 224))
+
+    # Replaces PyTorch data loader (`torch.utils.data.Dataloader`)
+    image_size = 224
+
+    dataloader = Loader(
+        "/home/lefaudeux/Data/ImageNet/ds.beton",
         batch_size=BATCH,
         num_workers=NUM_WORKERS,
-        pin_memory=True,
+        order=OrderOption.RANDOM,
+        pipelines={
+            "image": [decoder, Cutout(image_size), ToTensor(), ToDevice(0)],
+            "label": [IntDecoder(), ToTensor(), ToDevice(0)],
+        },
     )
 
-    image_size = dm.size(-1)  # 32 for CIFAR
-    num_classes = dm.num_classes  # 10 for CIFAR
+    num_classes = 1000
+    steps = len(dataloader) // REF_BATCH * MAX_EPOCHS
 
     # compute total number of steps
     batch_size = BATCH * GPUS
-    steps = dm.num_samples // REF_BATCH * MAX_EPOCHS
     lm = EfficientFormer(
         steps=steps,
         image_size=image_size,
@@ -176,10 +183,11 @@ if __name__ == "__main__":
     trainer = pl.Trainer(
         gpus=GPUS,
         max_epochs=MAX_EPOCHS,
-        precision=16,
+        precision=32,
         accumulate_grad_batches=REF_BATCH // BATCH,
+        checkpoint_callback=True,
     )
-    trainer.fit(lm, dm)
+    trainer.fit(lm, dataloader)
 
     # check the training
-    trainer.test(lm, datamodule=dm)
+    # trainer.test(lm, datamodule=dm)
